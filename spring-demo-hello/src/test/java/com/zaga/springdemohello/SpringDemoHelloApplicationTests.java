3a91099c4f9aaf47cd6597f4a164f1ff99492288
Ragamalya <ragamalya295@gmail.com>
2024-04-28 23:02:33 +0530

    Added SpringBoot App

---
+++ spring-demo-hello/src/test/java/com/zaga/springdemohello/SpringDemoHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaga.springdemohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
